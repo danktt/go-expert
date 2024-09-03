ff04938732b2e522c7be031f1afaabaa812d4553
Danilo Miranda <d.miranda@cfp.energy>
2024-09-02 22:47:55 -0300

    inferencia de tipo

--- day1/main.go
+++ day1/main.go
@@ -1,5 +1,23 @@
 package main
 
+const scope = "global"
+
+var (
+	a bool   = true
+	b int    = 10
+	c string = "Hello, World!"
+)
+
 func main() {
+	// Inferencia de tipo, nao precisa declarar o tipo da variavel
+	variavel := "variavel" // só pode ser usada dentro da função
+	println(variavel)
 	println("Hello, World!")
+
+	println(a)
+
+	a = true
+	println(a)
+
+	println(scope)
 }
